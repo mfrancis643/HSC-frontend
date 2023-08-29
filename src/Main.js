9e4d7c0ef547771e1506d2acb62cd786abf166a8
mfrancis643 <mfrancis643@googlemail.com>
2023-08-29 21:45:32 +0100

    Added Balance

--- src/Main.js
+++ src/Main.js
@@ -15,16 +15,9 @@ import CustomSection from "./components/CustomSection/CustomSection";
 import Description from "./components/Description/Description";
 
 const Main = () => {
-    const [isLoading, setIsLoading] = useState(false);
     const [bankBalance, setBankBalance] = useState(1000);
     const [resultsOpen, setResultsOpen] = useState(false);
     const [results, setResults] = useState({});
-    const [historicalYearInterestPairData, setHistoricalYearInterestPairData] = useState({})
-    const [customYearInterestPairData, setCustomYearInterestPairData] = useState({})
-
-    useEffect(() => {
-        console.log(results)
-    }, [results])
 
     const sendButtonClick = (yearInterestPayload) => {
 
@@ -46,6 +39,15 @@ const Main = () => {
             <div className="spacerMargin">
                 <div className="main">
                     <Description/>
+                        <div className="row">
+                            <span className="label">
+                                <TextField id="standard-basic"
+                                           label="Initial Bank Balance"
+                                           value={bankBalance}
+                                           onChange={(e) => setBankBalance(e.target.value)}
+                                />
+                            </span>
+                        </div>
 
                     <Grid container>
                         <Grid item xs={5.5}>
@@ -64,39 +66,6 @@ const Main = () => {
 
                     </Grid>
                 </div>
-                {/*<div className="main">*/}
-                {/*    */}
-                {/*    <div className="row">*/}
-                {/*        <span className="label">*/}
-                {/*            <TextField id="standard-basic"*/}
-                {/*                       label="Initial Bank Balance"*/}
-                {/*                       value={bankBalance}*/}
-                {/*                       onChange={(e) => setBankBalance(e.target.value)}*/}
-                {/*            />*/}
-                {/*        </span>*/}
-                {/*    </div>*/}
-                {/*    <div className="row">*/}
-                {/*        <span className="label" style={{padding:"10px"}}>Use Custom Data:</span>*/}
-                {/*        <span className="subComponent">*/}
-                {/*            <Switch checked={isCustomData}*/}
-                {/*                    onChange={() => setIsCustomData(!isCustomData)}*/}
-                {/*            />*/}
-                {/*        </span>*/}
-                {/*    </div>*/}
-                {/*    <HistoricalDataAccordion*/}
-                {/*        expanded={!isCustomData}*/}
-                {/*        disabled={isCustomData}*/}
-                {/*        setYearValuePairPayload={(newPayload) => {setHistoricalYearInterestPairData(newPayload)}}*/}
-                {/*    />*/}
-                {/*    <CustomDataAccordion*/}
-                {/*        expanded={isCustomData}*/}
-                {/*        disabled={!isCustomData}*/}
-                {/*        setYearValuePairPayload={(newPayload) => {setCustomYearInterestPairData(newPayload)}}*/}
-                {/*    />*/}
-
-                {/*    {isLoading ? "Loading...": null}*/}
-                {/*    <Button style={{margin: "20px", fontSize: "20px"}} variant="contained" size={'large'} onClick={() => sendButtonClick()}>Get Results</Button>*/}
-                {/*</div>*/}
                 {resultsOpen ? (
                     <ResultsModal open={resultsOpen} closeCall={() => setResultsOpen(false)} content={results}/>
                     ):(
